82374dc1e0750500510ed12a3d22b251ae4fc1f7
Filip Bártek <filip.bartek@hotmail.com>
2014-09-21 21:15:57 +0200

    Add srp test wiki_marriages
    Watch out: takes quite long (around 20 seconds) in the current
    implementation on my computer.

--- srp.pl
+++ srp.pl
@@ -45,6 +45,17 @@ test(wiki_6, [true(Partners == [6,4,5,2,3,1])]) :-
     [5,2,3,6,1],
     [3,1,2,4,6],
     [5,1,3,4,2]], Partners).
+%Source: http://en.wikipedia.org/wiki/File:Gale-Shapley.gif
+test(wiki_marriages, [true(Partners == [5,8,7,6,1,4,3,2])]) :-
+  srp([
+    [5,7,6,8],
+    [7,8,5,6],
+    [8,6,7,5],
+    [7,6,5,8],
+    [2,1,3,4],
+    [4,1,2,3],
+    [1,3,2,4],
+    [2,3,1,4]], Partners).
 :- end_tests(srp).
 
 %Constrains Partners according to Preferences
