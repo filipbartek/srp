72092614c1690065d8c681da59b5777ae64b35d3
Filip Bártek <filip.bartek@hotmail.com>
2014-09-21 21:27:40 +0200

    Add srp tests kleinberg_1 and kleinberg_2

--- srp.pl
+++ srp.pl
@@ -57,6 +57,15 @@ test(wiki_marriages, [true(Partners == [5,8,7,6,1,4,3,2])]) :-
     [4,1,2,3],
     [1,3,2,4],
     [2,3,1,4]], Partners).
+%Source: Tardos, Kleinberg: Stable matchings
+test(kleinberg_1, [true(Partners == [3,4,1,2])]) :-
+  srp([[3,4],[3,4],[1,2],[1,2]], Partners).
+%Source: Tardos, Kleinberg: Stable matchings
+%Has two solutions:
+%[3,4,1,2]
+%[4,3,2,1]
+test(kleinberg_2, [true]) :-
+  srp([[3,4],[4,3],[2,1],[1,2]], _Partners).
 :- end_tests(srp).
 
 %Constrains Partners according to Preferences
