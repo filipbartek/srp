565967eb91af35b77e42d12107c453379460a4d1
Filip Bártek <filip.bartek@hotmail.com>
2014-09-21 21:28:09 +0200

    srp.pl: Reformat for better readability - insert blank lines

--- srp.pl
+++ srp.pl
@@ -18,6 +18,7 @@
   end_tests/1
 ]).
 
+
 %srp(+Preferences, ?Partners)
 srp(Preferences, Partners) :-
   length(Preferences, N),
@@ -27,16 +28,20 @@ srp(Preferences, Partners) :-
   labeling([], Partners).
 
 :- begin_tests(srp).
+
 test(n0, [true(Partners == [])]) :- srp([], Partners).
 test(n1, [true(Partners == [1])]) :- srp([[1]], Partners).
 test(n2, [true(Partners == [2,1])]) :- srp([[2,1], [1,2]], Partners).
+
 %Source: http://en.wikipedia.org/wiki/Stable_roommates_problem#Solution
 test(fail_wikipedia, [fail]) :-
   srp([[2,3,4], [3,1,4], [1,2,4], [1,2,3]], _Partners).
+
 test(friends_fail, [fail]) :-
   srp([[3,4,2], [3], [1,2,4], [3,1,2]], _Partners).
 test(friends_true, true(Partners == [3,4,1,2])) :-
   srp([[3,4,2], [3,4], [1,2,4], [3,1,2]], Partners).
+
 %Source: http://en.wikipedia.org/wiki/Stable_roommates_problem#Example
 test(wiki_6, [true(Partners == [6,4,5,2,3,1])]) :-
   srp([
@@ -46,6 +51,7 @@ test(wiki_6, [true(Partners == [6,4,5,2,3,1])]) :-
     [5,2,3,6,1],
     [3,1,2,4,6],
     [5,1,3,4,2]], Partners).
+
 %Source: http://en.wikipedia.org/wiki/File:Gale-Shapley.gif
 test(wiki_marriages, [true(Partners == [5,8,7,6,1,4,3,2])]) :-
   srp([
@@ -57,16 +63,20 @@ test(wiki_marriages, [true(Partners == [5,8,7,6,1,4,3,2])]) :-
     [4,1,2,3],
     [1,3,2,4],
     [2,3,1,4]], Partners).
+
 %Source: Tardos, Kleinberg: Stable matchings
 test(kleinberg_1, [true(Partners == [3,4,1,2])]) :-
   srp([[3,4],[3,4],[1,2],[1,2]], Partners).
+
 %Source: Tardos, Kleinberg: Stable matchings
 %Has two solutions:
 %[3,4,1,2]
 %[4,3,2,1]
 test(kleinberg_2, [true]) :-
   srp([[3,4],[4,3],[2,1],[1,2]], _Partners).
+
 :- end_tests(srp).
+
 
 %Constrains Partners according to Preferences
 %stable(+Preferences, ?Partners)
