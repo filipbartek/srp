4f6ce6ca25ad1c51ae194b194f20fc0b69990b59
Filip Bártek <filip.bartek@hotmail.com>
2014-09-22 21:46:05 +0200

    Add NaturalDocs build script "build.bat"
    The Windows Batch script expects the environment variable NATURALDOCS to
    hold path to a directory where NaturalDocs 1.52 is installed. When run,
    it generates documentation from files in directory "src". The entry file
    to the doucmentation is "NaturalDocs/output/index.html".

---
+++ NaturalDocs/build.bat
@@ -0,0 +1,32 @@
+setlocal
+
+rem Expects:
+rem Environment variable NATURALDOCS points to NaturalDocs directory.
+rem Directory %INPUT_REL% exists.
+
+set INPUT_REL=..\src
+set OUTPUT_REL=output
+set PROJECT_REL=project
+
+pushd %INPUT_REL%
+set INPUT_ABS=%CD%
+popd
+
+mkdir %OUTPUT_REL%
+pushd %OUTPUT_REL%
+set OUTPUT_ABS=%CD%
+popd
+
+mkdir %PROJECT_REL%
+pushd %PROJECT_REL%
+set PROJECT_ABS=%CD%
+popd
+
+pushd %NATURALDOCS%
+NaturalDocs^
+ --input "%INPUT_ABS%"^
+ --output HTML "%OUTPUT_ABS%"^
+ --project "%PROJECT_ABS%"
+popd
+
+endlocal
