b25130940557d330dd75f01e2af9bdc855f86cf6
Filip Bártek <filip.bartek@hotmail.com>
2014-09-28 17:05:12 +0200

    analyze.R: Update to conform to new performance.pl output; various improvements

--- src/analyze.R
+++ src/analyze.R
@@ -6,32 +6,36 @@ colClasses = list(
 size = "integer",
 seed = "integer",
 executions = "integer",
-strategy = "factor",
-assumptions = "integer",
 solutions = "integer",
-runtime_one = "numeric",
-walltime_one = "numeric",
-runtime_all = "numeric",
-walltime_all = "numeric")
+leftmost.first.assumptions = "integer",
+leftmost.first.runtime = "integer",
+leftmost.first.walltime = "integer",
+leftmost.all.runtime = "integer",
+leftmost.all.walltime = "integer",
+ff.first.assumptions = "integer",
+ff.first.runtime = "integer",
+ff.first.walltime = "integer",
+ff.all.runtime = "integer",
+ff.all.walltime = "integer",
+ffc.first.assumptions = "integer",
+ffc.first.runtime = "integer",
+ffc.first.walltime = "integer",
+ffc.all.runtime = "integer",
+ffc.all.walltime = "integer")
 
 data <- read.csv(file = resultsFile, colClasses = colClasses, fileEncoding = "UTF-8")
 
-# Discard invalid columns
-data <- subset(data, select = -c(runtime_all, walltime_all))
-
-# Found at least solutions?
-# Otherwise, assumptions and times are invalid.
+# Mark trials with solved (i.e. soluble) instances
 data <- transform(data, solved = (solutions > 0))
 
 
 # Plot portion of solved instances
-dataSolved <- transform(data, instances = 0)
-dataSolved <- aggregate(dataSolved["instances"], by=dataSolved[c("size","solved")], FUN=length)
-dataSolved <- transform(dataSolved, instances = instances / 3)
-dataSolved <- subset(dataSolved, solved == TRUE, select = -c(solved))
+dataSolvedInst <- transform(data, instances = 0)
+dataSolvedInst <- aggregate(dataSolvedInst["instances"], by=dataSolvedInst[c("size","solved")], FUN=length)
+dataSolvedInst <- subset(dataSolvedInst, solved == TRUE, select = -c(solved))
 
 pdf(file = "soluble.pdf")
-barplot(dataSolved$instances, names.arg=dataSolved$size,
+barplot(dataSolvedInst$instances, names.arg=dataSolvedInst$size,
 	xlab="Size (participants)",ylab="Soluble instances (out of 10)",
 	yaxt="n")
 axis(2, c(0,5,7,8,9,10),
@@ -47,74 +51,81 @@ pdf(file = "executions.pdf")
 barplot(dataExecutions$x, names.arg=dataExecutions$size,
 	xlab="Size (participants)",ylab="Executions",
 	yaxt="n")
-axis(2, c(0,10,100),
-	c("0","10","100"))
+axis(2, c(0,1,10,100),
+	c("0","1","10","100"))
 title("Number of executions of solving procedure")
 dev.off()
 
 
-# Filter out trials with instances without solution
-dataValid <- subset(data, solved == TRUE, select = -c(solved))
-
-# Time for first solution per execution
-dataValid <- transform(dataValid, tf = runtime_one / executions)
+# Time per execution
+data <- transform(data, leftmost.first.runtime.mean = leftmost.first.runtime / executions)
+data <- transform(data, leftmost.first.walltime.mean = leftmost.first.walltime / executions)
+data <- transform(data, leftmost.all.runtime.mean = leftmost.all.runtime / executions)
+data <- transform(data, leftmost.all.walltime.mean = leftmost.all.walltime / executions)
+data <- transform(data, ff.first.runtime.mean = ff.first.runtime / executions)
+data <- transform(data, ff.first.walltime.mean = ff.first.walltime / executions)
+data <- transform(data, ff.all.runtime.mean = ff.all.runtime / executions)
+data <- transform(data, ff.all.walltime.mean = ff.all.walltime / executions)
+data <- transform(data, ffc.first.runtime.mean = ffc.first.runtime / executions)
+data <- transform(data, ffc.first.walltime.mean = ffc.first.walltime / executions)
+data <- transform(data, ffc.all.runtime.mean = ffc.all.runtime / executions)
+data <- transform(data, ffc.all.walltime.mean = ffc.all.walltime / executions)
 
 
 # Plot mean time
-tfMean <- aggregate(dataValid$tf, list(size = dataValid$size, strategy = dataValid$strategy), mean)
-tfMeanLeftmost <- subset(tfMean, strategy == "leftmost", select = -c(strategy))
-tfMeanFf <- subset(tfMean, strategy == "ff", select = -c(strategy))
-tfMeanFfc <- subset(tfMean, strategy == "ffc", select = -c(strategy))
+timeLeftmost <- aggregate(data$leftmost.first.runtime.mean, list(size = data$size), mean)
+timeFf <- aggregate(data$ff.first.runtime.mean, list(size = data$size), mean)
+timeFfc <- aggregate(data$ffc.first.runtime.mean, list(size = data$size), mean)
 
-#postscript(file = "time.eps")
 pdf(file = "time.pdf")
-plot(xlab="Size (participants)",ylab="Time (ms)",xlim=c(0,64),ylim=c(0,100),xaxt="n",
-	tfMeanLeftmost$size,tfMeanLeftmost$x,type="o",col="red")
-points(tfMeanFf$size,tfMeanFf$x,type="o",col="green")
-points(tfMeanFfc$size,tfMeanFfc$x,type="o",col="blue")
+plot(xlab="Size (participants)",ylab="Time (ms)",xlim=c(0,88),xaxt="n",
+	timeLeftmost$size,timeLeftmost$x,type="o",col="red")
+points(timeFf$size,timeFf$x,type="o",col="green")
+points(timeFfc$size,timeFfc$x,type="o",col="blue")
 legend("topleft", title="Strategy", c("leftmost", "ff", "ffc"),
 	fill=c("red", "green", "blue"))
-axis(1, c(0,1,2,4,8,16,24,32,40,48,56,64),
-	c("0","1","2","4","8","16","24","32","40","48","56","64"))
+axis(1, c(0,1,2,4,8,16,24,32,40,48,56,64,72,80,88),
+	c("0","1","2","4","8","16","24","32","40","48","56","64","72","80","88"))
 title("Average solving time")
 dev.off()
 
 
+# Extract solved instances
+dataSolved <- subset(data, solved == TRUE, select = -c(solved))
+
 # Plot mean assumptions
-assumptionsMean <- aggregate(dataValid$assumptions, list(size = dataValid$size, strategy = dataValid$strategy), mean)
-assumptionsMeanLeftmost <- subset(assumptionsMean, strategy == "leftmost", select = -c(strategy))
-assumptionsMeanFf <- subset(assumptionsMean, strategy == "ff", select = -c(strategy))
-assumptionsMeanFfc <- subset(assumptionsMean, strategy == "ffc", select = -c(strategy))
+assumptionsLeftmost <- aggregate(dataSolved$leftmost.first.assumptions, list(size = dataSolved$size), mean)
+assumptionsFf <- aggregate(dataSolved$ff.first.assumptions, list(size = dataSolved$size), mean)
+assumptionsFfc <- aggregate(dataSolved$ffc.first.assumptions, list(size = dataSolved$size), mean)
 
 pdf(file = "assumptions.pdf")
-plot(xlab="Size (participants)",ylab="Number of assumptions",xlim=c(0,64),ylim=c(0,240),xaxt="n",
-	assumptionsMeanLeftmost$size,assumptionsMeanLeftmost$x,type="o",col="red")
-points(assumptionsMeanFf$size,assumptionsMeanFf$x,type="o",col="green")
+plot(xlab="Size (participants)",ylab="Number of assumptions",xlim=c(0,88),ylim=c(0,240),xaxt="n",
+	assumptionsLeftmost$size,assumptionsLeftmost$x,type="o",col="red")
+points(assumptionsFf$size,assumptionsFf$x,type="o",col="green")
 # Number of assumptions is the same for ff and ffc
-#points(assumptionsMeanFfc$size,assumptionsMeanFfc$x,type="o",col="blue")
 #legend("topleft", title="Strategy", c("leftmost", "ff", "ffc"), fill=c("red", "green", "blue"))
 legend("topleft", title="Strategy", c("leftmost", "ff and ffc"), fill=c("red", "green"))
-axis(1, c(0,1,2,4,8,16,24,32,40,48,56,64),
-	c("0","1","2","4","8","16","24","32","40","48","56","64"))
+axis(1, c(0,1,2,4,8,16,24,32,40,48,56,64,72,80,88),
+	c("0","1","2","4","8","16","24","32","40","48","56","64","72","80","88"))
 title("Average number of assumptions in search for solution")
 dev.off()
 
 
 # Plot time boxplot
-pdf(file = "leftmost_time.pdf")
-boxplot(dataValid$tf~dataValid$size, subset = dataValid$strategy == "leftmost")
-title("Time with strategy leftmost")
+pdf(file = "ff_time.pdf")
+boxplot(data$ff.first.runtime.mean~data$size)
+title("Time with strategy ff")
 dev.off()
 
-boxplot(dataValid$tf~dataValid$size, subset = dataValid$strategy == "ff")
-boxplot(dataValid$tf~dataValid$size, subset = dataValid$strategy == "ffc")
+boxplot(data$leftmost.first.runtime.mean~data$size)
+boxplot(data$ffc.first.runtime.mean~data$size)
 
 # Plot assumptions boxplot
-pdf(file = "leftmost_assumptions.pdf")
-boxplot(dataValid$assumptions~dataValid$size, subset = dataValid$strategy == "leftmost")
-title("Assumptions with strategy leftmost")
+pdf(file = "ff_assumptions.pdf")
+boxplot(dataSolved$ff.first.assumptions~dataSolved$size)
+title("Assumptions with strategy ff")
 dev.off()
 
-boxplot(dataValid$assumptions~dataValid$size, subset = dataValid$strategy == "ff")
-boxplot(dataValid$assumptions~dataValid$size, subset = dataValid$strategy == "ffc")
+boxplot(dataSolved$leftmost.first.assumptions~dataSolved$size)
+boxplot(dataSolved$ffc.first.assumptions~dataSolved$size)
 
